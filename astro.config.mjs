d0f3065ea6b9c59712debb777507738b9dcb5618
Ardit Xhaferi <ardit@starlabspro.com>
2024-10-04 14:04:59 +0200

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github';
 
-export default defineConfig({
-  base: '/AveHR-Release-Notes/', // Replace with your repository name
-  adapter: github(),
-});
+// https://astro.build/config
+export default defineConfig({});
