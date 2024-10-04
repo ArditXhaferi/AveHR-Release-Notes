9575b9cdc7fa4332e9aeb629bf1ddf28afade345
Ardit Xhaferi <ardit@starlabspro.com>
2024-10-04 14:03:04 +0200

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,6 @@ import { defineConfig } from 'astro/config';
 import github from '@astrojs/github';
 
 export default defineConfig({
-  // Update the base path if your project is not deployed to the root
-  // For example, if it's deployed to github.com/user/repo, use 'repo' below
-  base: '/AveHR-Release-Notes/', 
-  adapter: github({
-    // if using a custom domain:
-    // domain: 'example.com'
-  }),
+  base: '/AveHR-Release-Notes/', // Replace with your repository name
+  adapter: github(),
 });
